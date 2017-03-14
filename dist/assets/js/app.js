9123628a45c1297ed1ac0c17572a450ebb769134
charra <tat86485689@gmail.com>
2017-03-14 22:06:00 +0200

    Update app.js

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3,85 +3,44 @@
 var map;
 var coordinates = {};
 
-function anAsyncCall() {  
+// function anAsyncCall() {  
 
-  function success(pos) {
-    coordinates.longitude = pos.coords.longitude;
-    coordinates.latitude = pos.coords.latitude;
+function success(pos) {
+  coordinates.longitude = pos.coords.longitude;
+  coordinates.latitude = pos.coords.latitude;
+  
+  console.log('Your current position is:');
+  console.log('Latitude : ' + coordinates.latitude);
+  console.log('Longitude: ' + coordinates.longitude);
+   //initMap();
+};
 
-    console.log('Your current position is:');
-    console.log('Latitude : ' + coordinates.latitude);
-    console.log('Longitude: ' + coordinates.longitude);
-
-    //initMap();
-  };
-
-  var position = navigator.geolocation.getCurrentPosition(success);
-  //position.then(function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: {
-  //       //lat: coordinates.latitude, lng: coordinates.longitude
-  //       lat: 50.01491644, lng: 36.22078389
-  //     },
-  //     zoom: 14,
-  //     zoomControl: true,
-  //     zoomControlOptions: {
-  //       style: google.maps.ZoomControlStyle.DEFAULT
-  //     },
-  //     disableDoubleClickZoom: true,
-  //     mapTypeControl: true,
-  //     mapTypeControlOptions: {
-  //       style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-  //     },
-  //     scaleControl: true,
-  //     scrollwheel: true,
-  //     panControl: true,
-  //     streetViewControl: true,
-  //     draggable: true,
-  //     overviewMapControl: true
-  //   });
-
-  //   // var image = 'assets/images/marker-map.png';
-  //   // var beachMarker = new google.maps.Marker({
-  //   //   position: {lat: 50.01491644, lng: 36.22078389},
-  //   //   map: map,
-  //   //   icon: image
-  //   // });
-  // });
-  //return position.then();
-}
+var position = navigator.geolocation.getCurrentPosition(success);
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        //lat: coordinates.latitude, lng: coordinates.longitude
-        lat: 50.01491644, lng: 36.22078389
-      },
-      zoom: 14,
-      zoomControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.DEFAULT
-      },
-      disableDoubleClickZoom: true,
-      mapTypeControl: true,
-      mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-      },
-      scaleControl: true,
-      scrollwheel: true,
-      panControl: true,
-      streetViewControl: true,
-      draggable: true,
-      overviewMapControl: true
-    });
-
-    // var image = 'assets/images/marker-map.png';
-    // var beachMarker = new google.maps.Marker({
-    //   position: {lat: 50.01491644, lng: 36.22078389},
-    //   map: map,
-    //   icon: image
-    // });
-  };	
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+    lat: coordinates.latitude, lng: coordinates.longitude
+    //lat: 50.01491644, lng: 36.22078389
+    },
+    zoom: 14,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.DEFAULT,
+    },
+    disableDoubleClickZoom: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+    },
+    scaleControl: true,
+    scrollwheel: true,
+    panControl: true,
+    streetViewControl: true,
+    draggable : true,
+    overviewMapControl: true
+  });
+}	
 
 
 
